081bf0da47a2462af9dd31c78d3fd9d0b38502ba
freyes <fran@codesai.com>
2024-11-20 15:11:14 +0000

    fix test

--- 03-refactoring-to-hexagonal-architecture/BirthdayGreetingsKata.Tests/OurDateTest.cs
+++ 03-refactoring-to-hexagonal-architecture/BirthdayGreetingsKata.Tests/OurDateTest.cs
@@ -10,7 +10,7 @@ public class OurDateTest
         var ourDate = new OurDate("1789/01/24");
         var sameDay = new OurDate("2001/01/24");
         var notSameDay = new OurDate("1789/01/25");
-        var notSameMonth = new OurDate("1789/02/25");
+        var notSameMonth = new OurDate("1789/02/24");
 
         Assert.That(ourDate.IsSameDay(sameDay), Is.True, "same");
         Assert.That(ourDate.IsSameDay(notSameDay), Is.False, "not same day");
